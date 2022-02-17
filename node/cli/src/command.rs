d180d476c6b6a60dde3385dc190d78e96812c764
Mostima <mostimasblunderbuss@protonmail.com>
2022-02-17 11:14:57 -0500

    subtensor parachain

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -78,15 +78,13 @@ impl SubstrateCli for Cli {
 				let chain_spec = Box::new(service::chain_spec::DummyChainSpec::from_json_file(path.clone())?)
 					as Box<dyn service::ChainSpec>;
 
-				if chain_spec.is_karura() {
-					#[cfg(feature = "with-nakamoto-runtime")]
-					{
-						Box::new(chain_spec::karura::ChainSpec::from_json_file(path)?)
-					}
-
-					#[cfg(not(feature = "with-nakamoto-runtime"))]
-					return Err(service::KARURA_RUNTIME_NOT_AVAILABLE.into());
+				#[cfg(feature = "with-nakamoto-runtime")]
+				{
+					Box::new(chain_spec::karura::ChainSpec::from_json_file(path)?)
 				}
+
+				#[cfg(not(feature = "with-nakamoto-runtime"))]
+				return Err(service::KARURA_RUNTIME_NOT_AVAILABLE.into());
 		})
 	}
 
